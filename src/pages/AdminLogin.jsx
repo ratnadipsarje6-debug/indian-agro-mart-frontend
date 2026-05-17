1bdd0e2edb46124dfdc0040c292c3cf21320f5eb
piyushsarje24-coder <piyushsarje24@gmail.com>
2026-05-17 13:57:54 +0530

    fix mobile admin login

--- src/pages/AdminLogin.jsx
+++ src/pages/AdminLogin.jsx
@@ -14,14 +14,16 @@ const handleLogin = async (e) => {
   e.preventDefault();
 
   try {
-  const response = await fetch("http://localhost:5000/api/users/login", {
+  const response = await fetch("https://indian-agro-mart-backend.onrender.com/api/users/login", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
       email: adminId,
+      adminId:adminId,
       password,
+      role:"admin",
     }),
   });
 
